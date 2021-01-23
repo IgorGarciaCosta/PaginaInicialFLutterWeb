f2bb14e3de1bbbe9cac1b9324405a04b7b6afab0
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-23 16:08:23 -0300

    finalizando navbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Flutter Website Test',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
+      body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [Color.fromRGBO(195, 20, 50, 1.0), Color.fromRGBO(36, 11, 54, 1.0)]
-          ),
-
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(195, 20, 50, 1.0),
+                Color.fromRGBO(36, 11, 54, 1.0)
+              ]),
         ),
         child: Column(
           children: <Widget>[
@@ -41,4 +39,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-

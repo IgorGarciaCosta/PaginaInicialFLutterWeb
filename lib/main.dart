18ee109444705faed6c25b3f8f84cf4e31415cc6
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-23 16:41:06 -0300

    add scroll à pagina

--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,27 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Color.fromRGBO(195, 20, 50, 1.0),
-                Color.fromRGBO(36, 11, 54, 1.0)
-              ]),
-        ),
-        child: Column(
-          children: <Widget>[
-            Navbar(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal:40.0),
-              child: LandingPage(),
-            )
-            
-          ],
-        ),
-      ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Color.fromRGBO(195, 20, 50, 1.0),
+                  Color.fromRGBO(36, 11, 54, 1.0)
+                ]),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Navbar(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 40.0),
+                  child: LandingPage(),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
